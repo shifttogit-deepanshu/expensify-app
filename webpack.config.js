9af6f91df46fde72181546affc3cfd25c162ca4e
Deepanshu Sharma <sharmadeepanshu.kaizen1@gmail.com>
2020-10-23 02:19:17 +0530

    made new dist directory for dev assets

--- webpack.config.js
+++ webpack.config.js
@@ -5,7 +5,7 @@ module.exports = (env)=>{
    return { 
     entry: "./src/app.js",
     output:{
-        path:path.join(__dirname,"public"),
+        path:path.join(__dirname,"public/dist/"),
         filename:'bundle.js'
     },
     module:{
@@ -43,8 +43,8 @@ module.exports = (env)=>{
     devtool:env==='production'?"source-map":"inline-source-map",
     devServer: {
         contentBase: path.join(__dirname, 'public'),
-        historyApiFallback:true
-       
+        historyApiFallback:true,
+        publicPath:'/dist/'       
       }
 
     }
